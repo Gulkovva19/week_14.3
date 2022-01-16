6cffbc23060fa3bdc4de84512f22a19ed5e0273b
Gulkovva19 <gulkova.sofya@yandex.ru>
2022-01-16 13:34:04 +0300

    homework

---
+++ index.js
@@ -0,0 +1,11 @@
+let changemefirst = () => {
+    let nature = document.getElementById
+        ('nature');
+    nature.src = "/assets/images/second.jpeg";
+}
+
+let changemesecond = () => {
+    let nature = document.getElementById
+        ('nature');
+    nature.src = "/assets/images/first.jpeg";
+}
